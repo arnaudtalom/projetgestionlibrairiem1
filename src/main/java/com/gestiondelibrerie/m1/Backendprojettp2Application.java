c29db9ba80361383a5b4423358bacf50143dccd8
talombouopdaarnaud <talombouopdaarnaud@gmail.com>
2021-05-15 13:40:26 +0200

    init commit

---
+++ src/main/java/com/gestiondelibrerie/m1/Backendprojettp2Application.java
@@ -0,0 +1,13 @@
+package com.gestiondelibrerie.m1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backendprojettp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Backendprojettp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestiondelibrerie/m1/Backendprojettp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestiondelibrerie.m1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backendprojettp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
